d66cf63a99ed38abd1e4fd0dd9a0d06464901373
Luis <luis.h.rios@unl.edu.ec>
2026-01-14 20:53:56 -0500

    Proyecto Inicilizado

---
+++ src/main/java/edu/unl/cc/paperShop/jakarta/hello/Hello.java
@@ -0,0 +1,14 @@
+package edu.unl.cc.paperShop.jakarta.hello;
+
+public class Hello {
+
+    private String name;
+
+    public Hello(String name) {
+        this.name = name;
+    }
+
+    public String getHello(){
+        return name;
+    }
+}

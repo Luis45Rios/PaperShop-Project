7662ea001d9881a4e6f6817111a4cc8c47004443
Mishell <mishell.castillo@unl.edu.ec>
2026-02-04 09:05:02 -0500

    Se agregó clase AuthorizationFilter

--- src/main/java/unl/edu/ec/papershop/view/security/UserSession.java
+++ src/main/java/unl/edu/ec/papershop/view/security/UserSession.java
@@ -7,40 +7,58 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.constraints.NotNull;
 import unl.edu.ec.papershop.business.SecurityFacade;
 import unl.edu.ec.papershop.domain.security.ActionType;
+import unl.edu.ec.papershop.domain.security.Role;
 import unl.edu.ec.papershop.domain.security.User;
 
 
-import javax.management.relation.Role;
 import java.io.Serial;
-import java.io.Serializable;
 import java.util.Set;
 import java.util.logging.Logger;
 
 @Named
 @SessionScoped
-public class UserSession implements Serializable {
+public class UserSession implements java.io.Serializable{
 
+    @Serial
     private static final long serialVersionUID = 1L;
+
+    private static final Logger logger = Logger.getLogger(UserSession.class.getName());
+
+    @Inject
+    private SecurityFacade securityFacade;
 
     private User user;
 
-    public void postLogin(User user) {
+    public void postLogin(@NotNull User user) throws EntityNotFoundException {
+        logger.info("User logged in: " + user.getName());
         this.user = user;
+        Set<Role> roles = securityFacade.findRolesWithPermissionByUser(this.user.getId());
+        user.setRoles(roles);
     }
 
-    public void logout() {
-        this.user = null;
+    public boolean hasPermissionForPage(String pagePath) {
+        return this.hasPermission(pagePath, ActionType.READ);
     }
 
-    public boolean isLoggedIn() {
-        return user != null;
+    public boolean hasPermission(String resource, ActionType action) {
+        if (resource.equals("/dashboard.xhtml")){
+            return true;
+        }
+        return user.getRoles().stream()
+                .flatMap(role -> role.getPermissions().stream())
+                .anyMatch(permission -> permission.matchWith(resource, action));
+    }
+
+    public boolean hasRole(@NotNull String roleName){
+        if (user == null){
+            return false;
+        }
+        return user.getRoles().stream()
+                .anyMatch(role -> role.getName().equals(roleName));
     }
 
     public User getUser() {
         return user;
     }
 
-    public void setUser(User user) {
-        this.user = user;
-    }
 }
